3aa3e271c8e28a8ee7d5f9f1ea7ad0ba297a3682
khan-fatima <aarnavsangekar@gmail.com>
2023-11-07 16:59:10 +0530

    Fixed Delete and Add Buttons and Added Table Borders

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 function App() {
   const [data, setData] = useState([]);
   const [newRow, setNewRow] = useState({ serial: '', name: '', city: '', cgpa: '', phone: '' });
-
+  // 
   const fetchTableData = async () => {
-    // Make an API request to fetch table data from the server (Step 4).
-    const response = await fetch('/api/data');
+    const response = await fetch('http://localhost:5000/api/data');
     const tableData = await response.json();
     setData(tableData);
   };
@@ -18,7 +17,7 @@ function App() {
 
   const handleAddRow = async () => {
     // Make an API request to add a new row to the database (Step 5).
-    await fetch('/api/data', {
+    await fetch('http://localhost:5000/api/data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -35,7 +34,7 @@ function App() {
 
   const handleDeleteRow = async (id) => {
     // Make an API request to delete a row from the database (Step 6).
-    await fetch(`/api/data/${id}`, {
+    await fetch(`http://localhost:5000/api/data/${id}`, {
       method: 'DELETE',
     });
 
@@ -45,31 +44,31 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Dynamic Table</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Serial Number</th>
-            <th>Name</th>
-            <th>City</th>
-            <th>CGPA</th>
-            <th>Phone Number</th>
-            <th>Action</th>
+    <h1 style={{ fontWeight: 'bold' }}>Dynamic Table</h1>
+    <table style={{ border: '1px solid #ccc', borderCollapse: 'collapse' }}>
+      <thead>
+        <tr>
+          <th style={{ border: '1px solid #ccc', padding: '8px', fontWeight: 'bold' }}>Serial Number</th>
+          <th style={{ border: '1px solid #ccc', padding: '8px', fontWeight: 'bold' }}>Name</th>
+          <th style={{ border: '1px solid #ccc', padding: '8px', fontWeight: 'bold' }}>City</th>
+          <th style={{ border: '1px solid #ccc', padding: '8px', fontWeight: 'bold' }}>CGPA</th>
+          <th style={{ border: '1px solid #ccc', padding: '8px', fontWeight: 'bold' }}>Phone Number</th>
+          <th style={{ border: '1px solid #ccc', padding: '8px', fontWeight: 'bold' }}>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row) => (
+          <tr key={row._id}>
+            <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.serial}</td>
+            <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.name}</td>
+            <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.city}</td>
+            <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.cgpa}</td>
+            <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.phone}</td>
+            <td style={{ border: '1px solid #ccc', padding: '8px' }}>
+              <button onClick={() => handleDeleteRow(row._id)}>Delete</button>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {data.map((row) => (
-            <tr key={row._id}>
-              <td>{row.serial}</td>
-              <td>{row.name}</td>
-              <td>{row.city}</td>
-              <td>{row.cgpa}</td>
-              <td>{row.phone}</td>
-              <td>
-                <button onClick={() => handleDeleteRow(row._id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
+        ))}
           <tr>
             <td>
               <input
--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+require('dotenv').config()
+
+// MongoDB Atlas connection string
+const mongoURI = process.env.MONGO_URI;
+
 const port = 5000;
 
-mongoose.connect('mongodb://localhost/tableData', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://ioeserveruser:hvXjHwHKgWfZEF4k@cluster0.clj22dc.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB Atlas: ' + err);
+  });
 
 const dataSchema = new mongoose.Schema({
   serial: String,
@@ -20,6 +29,7 @@ const dataSchema = new mongoose.Schema({
 const Data = mongoose.model('Data', dataSchema);
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/api/data', async (req, res) => {
   const data = await Data.find();
@@ -28,13 +38,14 @@ app.get('/api/data', async (req, res) => {
 
 app.post('/api/data', async (req, res) => {
   const newRow = new Data(req.body);
+  console.log(newRow);
   await newRow.save();
   res.json(newRow);
 });
 
 app.delete('/api/data/:id', async (req, res) => {
   const id = req.params.id;
-  await Data.findByIdAndRemove(id);
+  await Data.findByIdAndDelete(id);
   res.json({ message: 'Deleted' });
 });
 
